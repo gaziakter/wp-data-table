4858e0a4bf5e7f0d76d8eee381be1ba485ef76b2
Gazi Akter <gaziakterdev@gmail.com>
2024-01-05 16:54:11 +0600

    update data

--- dataset.php
+++ dataset.php
@@ -42,11 +42,5 @@ $data = [
         'email'=>'john@wick.com',
         'age'=>26,
         'sex'=>'M'
-    ],[
-		'id'=>6,
-		'name'=>'Gazi Akter',
-		'email'=>'gazi@doe.com',
-		'age'=>38,
-		'sex'=>'Ml'
-	]
+    ]
 ];
--- wp-data-table.php
+++ wp-data-table.php
@@ -14,62 +14,96 @@
  * Text Domain:       data-table
  * Domain Path:       /languages
  */
+require_once "class.persons-table.php";
 
- include_once "class.persons-table.php";
+function tabledata_load_textdomain() {
+	load_plugin_textdomain( 'tabledata_example', false, dirname( __FILE__ ) . "/languages" );
+}
 
+add_action( "plugins_loaded", "tabledata_load_textdomain" );
 
- /**
-  * Load textdomain
-  */
- function wdtable_textdomain(){
-    load_textdomain( "data-table", false, dirname(__FILE__)."/languages" );
- }
- add_action( "plugins_loaded", "wdtable_textdomain" );
+function datatable_admin_page() {
+	add_menu_page(
+		__( 'Data Table', 'tabledata' ),
+		__( 'Data Table', 'tabledata' ),
+		'manage_options',
+		'datatable',
+		'datatable_display_table'
+	);
+}
 
- /**
-  * Creation admin menu
-  */
- function wdtable_admin_menu(){
-    add_menu_page(
-        __('Data Table', 'data-table'),
-        __('Data Table', 'data-table'),
-        'manage_options',
-        'datatable',
-        'wdtable_display_table'
-    );
+function datatable_search_by_name( $item ) {
+	$name        = strtolower( $item['name'] );
+	$search_name = sanitize_text_field( $_REQUEST['s'] );
+	if ( strpos( $name, $search_name ) !== false ) {
+		return true;
+	}
 
-    function datatable_search_by_name($item){
-        $name = strtolower($item['name']);
-        $search_name = sanitize_text_field( $_REQUEST['s'] );
-        if(strpos($name, $search_name) !== false){
+	return false;
+}
+
+function datatable_filter_sex($item){
+    $sex = $_REQUEST['filter_s']??'all';
+    if('all'==$sex){
+        return true;
+    }else{
+        if( $sex==$item['sex']){
             return true;
         }
-        return false;
     }
+    return false;
+}
 
-    function wdtable_display_table(){
-        include_once "dataset.php";
+function datatable_display_table() {
+	include_once "dataset.php";
+	$orderby = $_REQUEST['orderby'] ?? '';
+	$order   = $_REQUEST['order'] ?? '';
+	if ( isset( $_REQUEST['s'] ) && !empty($_REQUEST['s']) ) {
+		$data = array_filter( $data, 'datatable_search_by_name' );
+	}
 
-        if(isset($_REQUEST['s'])){
-            $data = array_filter($data, 'datatable_search_by_name');
-        } 
+	if ( isset( $_REQUEST['filter_s'] ) && !empty($_REQUEST['filter_s']) ) {
+		$data = array_filter( $data, 'datatable_filter_sex' );
+	}
 
-        $table = new Persons_Table();
-        $table->set_data($data);
-        $table->prepare_items();
-        ?>
-        <div class="wrap">
-            <h2><?php _e("Persons", "data-table"); ?></h2>
-            <form method="GET">
-                <?php
-                    $table->search_box('search', 'search_id');
-                    $table->display();
-                ?>
-                <input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>" >
-            </form>
-        </div>
-        <?php
-    }
+	$table = new Persons_Table();
+	if ( 'age' == $orderby ) {
+		if ( 'asc' == $order ) {
+			usort( $data, function ( $item1, $item2 ) {
+				return $item2['age'] <=> $item1['age'];
+			} );
+		} else {
+			usort( $data, function ( $item1, $item2 ) {
+				return $item1['age'] <=> $item2['age'];
+			} );
+		}
+	} else if ( 'name' == $orderby ) {
+		if ( 'asc' == $order ) {
+			usort( $data, function ( $item1, $item2 ) {
+				return $item2['name'] <=> $item1['name'];
+			} );
+		} else {
+			usort( $data, function ( $item1, $item2 ) {
+				return $item1['name'] <=> $item2['name'];
+			} );
+		}
+	}
+	$table->set_data( $data );
 
- }
- add_action('admin_menu', 'wdtable_admin_menu');
+	$table->prepare_items();
+	?>
+    <div class="wrap">
+        <h2><?php _e( "Persons", "tabledata" ); ?></h2>
+        <form method="GET">
+			<?php
+			$table->search_box( 'search', 'search_id' );
+			$table->display();
+			?>
+            <input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>">
+        </form>
+    </div>
+	<?php
+
+}
+
+add_action( "admin_menu", "datatable_admin_page" );
